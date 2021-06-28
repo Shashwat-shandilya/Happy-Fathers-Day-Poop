7955f3829ceaf09882e857eec373beb602628e83
Shashwat-shandilya <72491585+Shashwat-shandilya@users.noreply.github.com>
2021-06-28 10:47:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+var container = document.querySelector('.container');
+var value1 = document.querySelector('.input');
+var add1 = document.querySelector('.add');
+
+class item{
+    constructor(name){
+        this.create(name)
+    }
+    create(name){
+        var l1 = document.createElement('div');
+
+        l1.classList.add('item');
+
+        var input= document.createElement('input');
+        input.type="text";
+        input.disabled=true;
+        input.value=name;
+        input.style="margin-bottom: 10px; font-size: 20px; width: 380px; background: white; border:none; outline:none; color: green;"
+        input.classList.add('input');
+
+        var remove = document.createElement('button');
+        remove.classList.add('remove');
+        remove.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+        remove.addEventListener('click',()=>this.remove(l1));
+
+
+        container.appendChild(l1);
+        l1.appendChild(input);
+        l1.appendChild(remove);
+    }
+
+    remove(l1){
+        container.removeChild(l1);
+    }
+}
+
+add1.addEventListener('click', check);
+window.addEventListener('keydown',(e)=>{
+
+    if(e.which==13){
+        check();
+    }
+})
+
+function check(){
+    if(value1.value != ""){
+        new item(value1.value);
+        value1.value="";
+    }else{
+        window.alert("please enter text in the input");
+    }
+}
+
+function removeAll(){
+    document.querySelector('.container').innerHTML = "";
+}
